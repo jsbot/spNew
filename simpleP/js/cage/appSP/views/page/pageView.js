da6475da56faa5fdb4d12e4f270c393d02178607
jsbot <jsbot0001@gmail.com>
2013-09-03 16:46:44 +0300

    1. add small optimization in connector.js(get send methods)  2. in pageView provide small refactoring  3. add pubsub object for next optimization

--- simpleP/js/cage/appSP/views/page/pageView.js
+++ simpleP/js/cage/appSP/views/page/pageView.js
@@ -1,78 +1,82 @@
 // Filename: views/project/list
 define([
-    'jquery',
-    'underscore',
-    'backbone',
-    'cage/appSP/collections/pageCollection',
-    // Using the Require.js text! plugin, we are loaded raw text
-    // which will be used as our views primary template
-    'text!cage/appSP/templates/page/page.html',
-    'handlebars',
-    'service/connector'
-], function($, _, Backbone, PageCollection, pageTemplate, Handlebars, connector){
+	'jquery',
+	'underscore',
+	'backbone',
+	'cage/appSP/collections/pageCollection',
+	// Using the Require.js text! plugin, we are loaded raw text
+	// which will be used as our views primary template
+	'text!cage/appSP/templates/page/page.html',
+	'handlebars',
+	'service/connector'
+], function ($, _, Backbone, PageCollection, pageTemplate, Handlebars, connector) {
 
 
-    pageView = Backbone.View.extend({
-        el: $('#container'),
-        template: Handlebars.compile(pageTemplate),
-        events: {
-            "click .test": "navigateToFriends" // Обработчик клика на кнопке "Проверить"
-        },
-        navigateToFriends: function(){
-            alert("navigate to friends");
-            appRouterInstance.navigate("friends",true);
-        },
-        addRepro: function (collection,reproArr){
+	pageView = Backbone.View.extend({
+		el: $('#container'),
+		template: Handlebars.compile(pageTemplate),
+		events: {
+			"click .test": "navigateToFriends" // Обработчик клика на кнопке "Проверить"
+		},
+		navigateToFriends: function () {
+			alert("navigate to friends");
+			appRouterInstance.navigate("friends", true);
+		},
+		addRepro: function (collection, reproArr) {
+			for (i = 0; i <= reproArr.length; i++) {
+				collection.add(reproArr[i]);
+			}
+			this.render();
+		},
 
-            for(i=0; i<=reproArr.length; i++){
-                collection.add(reproArr[i]);
-            }
+		render: function () {
+			// Using Underscore we can compile our template with data
 
-        },
+			var context = this.collection.models;
+			var html = this.template({numbers: context});
+			this.$el.html(html);
 
-        render: function(){
-		    // Using Underscore we can compile our template with data
+		},
+		initialize: function () {
+			var _this = this;
+			var conn = connector;
+			conn.getMessage('wellcome',function(data){
+				console.log(data);
+			});
+			_this.collection = new PageCollection();
+			conn.getMessage('news',function(data){
+				console.log(data);
+				_this.addRepro(_this.collection, data);
+			});
+			conn.sendMessage('message','{"id":"0001","collection":"friends"}');
 
-            var context = this.collection.models;
-            var html    = this.template({numbers: context});
-            this.$el.html( html );
-
-        },
-        initialize: function(){
-            var _this = this;
-            var conn = connector;
-            conn.on('wellcome', function (data) {
-                console.log(data);     alert('dsfdf');
-            });
-            conn.on('news', function (data) {
-                console.log(data);
-                _this.collection = new PageCollection();
-                _this.addRepro(_this.collection,data);
-            });
-
-
-            conn.emit('message', '{"id":"0001","collection":"friends"}');
-            console.log("send Friends reqiest");
-
-            this.testModel = {title: 'Title', text: 'Text'};
-
+/*			conn.on('wellcome', function (data) {
+				console.log(data);
+			});
+			_this.collection = new PageCollection();
+			conn.on('news', function (data) {
+				console.log(data);
+				_this.addRepro(_this.collection, data);
+			});
+            */
+			//conn.emit('message', '{"id":"0001","collection":"friends"}');
 
 
-            /*this.collection.add({
-                id: 1,
-                title: "html mockup2",
-                img: "img/1.jpg",
-                src: "/test.html"
-            });*/
-            /*this.collection.add({
-                id: 2,
-                title: "html mockup3",
-                img: "img/2.jpg",
-                src: "asdasdasdas/test.html"
-            });*/
-        }
+			/*this.collection.add({
+			 id: 1,
+			 title: "html mockup2",
+			 img: "img/1.jpg",
+			 src: "/test.html"
+			 });*/
+			/*this.collection.add({
+			 id: 2,
+			 title: "html mockup3",
+			 img: "img/2.jpg",
+			 src: "asdasdasdas/test.html"
+			 });*/
+		}
 
-    });
-    // Our module now returns our view
-    return pageView;
+	});
+	// Our module now returns our view
+	return pageView;
 });
--- simpleP/js/service/connector.js
+++ simpleP/js/service/connector.js
@@ -1,10 +1,28 @@
 // Filename: connector.js
 define([
-    'jquery',
-    'socketio'
-], function($, io){
+	'jquery',
+	'socketio'
+], function ($, io) {
 
-    var socket = io.connect('http://127.0.0.1:8880');
+	var socket = io.connect('http://127.0.0.1:8880');
 
-    return socket;
+	/*conn.on('wellcome', function (data) {
+		console.log(data);
+	});*/
+
+
+	//conn.emit('message', '{"id":"0001","collection":"friends"}');
+
+
+	socket.getMessage = function(messageType, callback){
+		socket.on(messageType, function (data){
+			callback(data);
+		});
+	}
+	socket.sendMessage = function(messageType, data, callback){
+		socket.emit('message', data);
+		//subscribe for event
+	}
+
+	return socket;
 });
